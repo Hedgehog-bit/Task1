7d962b4d2676578540b187a33712cecb0da17f68
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-05-08 18:55:49 +0300

    Add files via upload

---
+++ Консоль/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону равнобедркнного треугольника");
+            double side = double.Parse(Console.ReadLine());
+            double r = side / Math.Sqrt(3);
+            Console.WriteLine(r);
+
+            //====================================Задание1.2======================================//
+
+            Console.Write("Введите a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите c=");
+            double c = double.Parse(Console.ReadLine());
+            if (System.Math.Pow(c, 2) == System.Math.Pow(a, 2) + System.Math.Pow(b, 2))
+            {
+                Console.Write("Треугольник прямоугольный");
+            }
+            else
+                Console.WriteLine("Треугольник не прямоугольный");
+
+            Console.ReadKey();
+        }
+    }
+}
