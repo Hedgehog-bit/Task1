a4387a094413b6300ed0f92a17564a198d8b0fa5
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-05-08 18:57:01 +0300

    Add files via upload

---
+++ WF/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Задание1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Output1 = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Input3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Input2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Output2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(584, 359);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Output1);
+            this.tabPage1.Controls.Add(this.Input);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(576, 333);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Задание1.1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Output1
+            // 
+            this.Output1.Location = new System.Drawing.Point(184, 266);
+            this.Output1.Name = "Output1";
+            this.Output1.Size = new System.Drawing.Size(196, 20);
+            this.Output1.TabIndex = 3;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(362, 46);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(100, 20);
+            this.Input.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите сторону равнобедркнного треугольника";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подсчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.Output2);
+            this.tabPage2.Controls.Add(this.Input1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.Input2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.Input3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(576, 333);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Задание1.2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Определить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Input3
+            // 
+            this.Input3.Location = new System.Drawing.Point(390, 110);
+            this.Input3.Name = "Input3";
+            this.Input3.Size = new System.Drawing.Size(100, 20);
+            this.Input3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите c=";
+            // 
+            // Input2
+            // 
+            this.Input2.Location = new System.Drawing.Point(390, 72);
+            this.Input2.Name = "Input2";
+            this.Input2.Size = new System.Drawing.Size(100, 20);
+            this.Input2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(270, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите b=";
+            // 
+            // Input1
+            // 
+            this.Input1.Location = new System.Drawing.Point(390, 37);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(100, 20);
+            this.Input1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите a=";
+            // 
+            // Output2
+            // 
+            this.Output2.Location = new System.Drawing.Point(140, 251);
+            this.Output2.Name = "Output2";
+            this.Output2.Size = new System.Drawing.Size(283, 20);
+            this.Output2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Задание№1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Output1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Input2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Input3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Output2;
+    }
+}
+
---
+++ WF/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double side = double.Parse(this.Input.Text);
+            double r = side / Math.Sqrt(3);
+            this.Output1.Text = r.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {  
+            double a = double.Parse(this.Input1.Text);
+            double b = double.Parse(this.Input2.Text);
+            double c = double.Parse(this.Input3.Text);
+            if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+            {
+                this.Output2.Text = "Треугольник прямоугольный";
+            }
+            else
+                this.Output2.Text = "Треугольник не прямоугольный";
+        }
+    }
+}
